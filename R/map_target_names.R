22ad7ddc83d1694600e66c1941287f5fa4d64c87
YantingHuMn <cynthia.huyanting@gmail.com>
2025-11-25 00:42:26 -0500

    add data

---
+++ R/map_target_names.R
@@ -0,0 +1,16 @@
+map_target_names <- function(target_pair_list, target_pair_mapping_df_path = NULL, from = "targets", to = "shorthand" ) {
+  if (is.null(target_pair_mapping_df_path)) {
+    target_pair_mapping_df_path <- target_pair_short_hand
+  }
+
+  target_pair_mapping_df <- read.table(target_pair_mapping_df_path, header = TRUE, check.names = FALSE)
+  cur_names <- target_pair_mapping_df[[from]]
+  new_names <- target_pair_mapping_df[[to]]
+  result <- target_pair_list
+  for (i in seq_along(cur_names)) {
+    cur_name <- cur_names[i]
+    new_name <- new_names[i]
+    result <- gsub(cur_name, new_name, result, fixed = TRUE)
+  }
+  return(result)
+}
