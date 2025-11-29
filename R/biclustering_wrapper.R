3c35d5b3d57bc52e4d2a95a26bdf852866100e35
Qingjie-Yu <yuqingjie1112@gmail.com>
2025-11-28 23:40:07 -0500

    Create biclustering wrapper

---
+++ R/biclustering_wrapper.R
@@ -0,0 +1,17 @@
+
+biclustering_wrapper <- function(cm_path, out_dir, apply_filter = TRUE, row_km = 15, col_km = 3, apply_annotation = TRUE) {
+    # Step1: Merge all the count matrix files
+    merged_cm_path <- merge_count_matrices(cm_path = cm_path, out_dir = out_dir)
+    # Step2: Filter highly variable regions
+    if (apply_filter) {
+        mav_path <- mav_screen(path = merged_cm_path, out_dir = out_dir, plot=FALSE)
+        f_cm_path <- informative_regions(input_hypervar_path=mav_path,input_CRF_orig_path=merged_cm_path,output_dir_path=out_dir, plot=FALSE)
+    } else {
+        f_cm_path <- merged_cm_path
+    }
+    # Step3: Biclustering & Annotation
+    apply_cluster_heatmap_advanced(count_matrix_file_path = f_cm_path, row_km = row_km, col_km = col_km,output_dir_path=out_dir)
+    if (apply_annotation) {
+        cat()
+    }
+}
